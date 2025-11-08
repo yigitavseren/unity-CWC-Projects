d4e5600017e75b9788cf418952e8862f6f911931
Yiğit Avseren <avseren.dev@gmail.com>
2025-11-09 01:42:10 +0300

    Local multiplayer projesi yapıldı.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,8 +7,8 @@ public class PlayerController : MonoBehaviour
     //Private Variables
     public float speed = 5.0f;
     public float turnSpeed;
-    public float horizontalInput;
-    public float forwardInput;
+    public string horizontalAxisName = "Horizontal";
+    public string verticalAxisName = "Vertical";
 
     // Start is called before the first frame update
     void Start()
@@ -20,12 +20,12 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         //This is where we get player input
-        horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis(horizontalAxisName);
+        float verticalInput = Input.GetAxis(verticalAxisName);
 
 
         //We move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
         //We turn the vehicle
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
